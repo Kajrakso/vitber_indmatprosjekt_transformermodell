5739bbf0180e525e95ce71eb9a690ac4240a6857
Kajrakso <ofjakob@gmail.com>
2024-03-06 12:30:45 +0100

    test_trained_network: tests model against test data

--- prosjekt_2_utlevert_kode/training_algorithm.py
+++ prosjekt_2_utlevert_kode/training_algorithm.py
@@ -13,25 +13,33 @@ from utils import onehot
 
 
 def alg4(
-    training_data,
-    network,
+    network: NeuralNetwork,
+    x: np.ndarray,
+    y: np.ndarray,
     loss_func,
-    alpha,
-    n_iter,
-    num_ints,
-    num_batches,
-    batch_size,
+    alpha: float,
+    n_iter: int,
+    num_ints: int,
 ) -> None:
-    """optimizes the paramaters in the network using the Adam algorithm"""
-    x = training_data["x_train"]
-    y = training_data["y_train"]
+    """optimizes the paramaters in the network using the Adam algorithm
+
+    Args:
+        network: The BOSS
+        x: (num_batches, batch_size, n), input data
+        y: (num_batches, batch_size, n), output data
+        loss_func
+        alpha: alpha paramater in the Adam algorithm (see p. 19, alg. 3)
+        n_iter: number of iterations before the training ends
+        num_ints: size of vocabulary (m in the project description)
+    """
+    num_batches = np.shape(x_train)[0]
 
     print("\nstart training...\n")
     print(f"{'iter. step':>15} | {'object function L':>15}")
     print("-" * 40)
     for i in range(n_iter):
-        L = np.zeros(batch_size)
-        for j in range(b):
+        L = np.zeros(num_batches)
+        for j in range(num_batches):
             Z = network.forward(onehot(x[j], m=num_ints))
             L[j] = loss_func.forward(y_hat=Z, y=y[j])
             grad_Z = loss.backward()
@@ -39,12 +47,46 @@ def alg4(
             # _ = network.step_gd(alpha)
             _ = network.step_adam(alpha)
 
-        # hopefully L will decrease as we optimize
+        # hopefully L will decrease as we optimize the params
         print(f"{i+1:>15} | {np.mean(L):>15.10f}")
     print("\nend of training...\n")
 
 
+def test_trained_network(
+        network: NeuralNetwork, x_test: np.ndarray, y_test: np.ndarray, num_ints: int
+) -> None:
+    """After having trained the network it should be tested on
+    data it was not trained on.
+
+    Args:
+        network: pre-trained network
+        x_test: (num_batches, batch_size, n)
+        y_test: (num_batches, batch_size, n)
+        num_ints: size of vocabulary (m in the project description)
+    """
+    batch_size = np.shape(x_test)[1]
+    n = np.shape(x_test)[-1]            # length of input x
+
+
+    print("\nstart testing...\n")
+    print(f"{'batch nr.':>15} | {'percentage correct':>15}")
+    print("-" * 40)
+    for i, x_batch in enumerate(x_test):
+        for _ in range(n):
+            Z = network.forward(onehot(x_batch, m=num_ints))
+            x_batch = np.insert(x_batch, -1, np.argmax(Z, axis=1)[:, -1], axis=1)
+        y = x_batch[:, r:]
+
+        num_correct = np.sum(np.all(y_test[i] == y, axis=1))
+        print(f"{i+1:>15} | {num_correct/batch_size:.3%}")
+    print("\nend of testing...\n")
+
+
 if __name__ == "__main__":
+    #
+    # this should probably be done in the notebook for handin
+    #
+
     # alpha parameter for Adam
     alpha = 0.01
 
@@ -66,6 +108,8 @@ if __name__ == "__main__":
     p = 15
     L = 2
 
+    n_iter = 20  # force the training to stop after n_iter steps.
+
     # initialize the network and the loss function
     softmax = Softmax()
     # feed_forward = [FeedForward(d=d,p=p) for _ in range(L)]
@@ -81,6 +125,7 @@ if __name__ == "__main__":
 
     loss = CrossEntropy()
 
+    # prepare training and test data for sorting
     training_data = get_train_test_sorting(
         length=r,
         num_ints=m,
@@ -88,16 +133,13 @@ if __name__ == "__main__":
         n_batches_train=b,
         n_batches_test=int(b / 2),
     )
+    x_train = training_data["x_train"]
+    y_train = training_data["y_train"]
+    x_test = training_data["x_test"]
+    y_test = training_data["y_test"]
 
-    n_iter = 20  # force the training to stop after n_iter steps.
+    # train the network
+    alg4(network, x_train, y_train, loss, alpha, n_iter, m)
 
-    alg4(
-        training_data,
-        network,
-        loss,
-        alpha,
-        n_iter,
-        num_ints=m,
-        num_batches=b,
-        batch_size=D,
-    )
+    # test against test data:
+    test_trained_network(network, x_test, y_test, m)
