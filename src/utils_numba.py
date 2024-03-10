7802f746cd7b4271bf427589a77b4717d64fa91b
tncrdk <thnchrdjk@gmail.com>
2024-03-10 15:25:20 +0100

    formatting

--- src/utils_numba.py
+++ src/utils_numba.py
@@ -1,28 +1,29 @@
 import numpy as np
 from numba import njit
 
+
 @njit
 def onehot_numba(x, m):
-    """ numba compatible onehot, other numba functions must use this onehot 
+    """numba compatible onehot, other numba functions must use this onehot
     Input:
     - x : np.array of integers with shape (b,n)
-             b is the batch size and 
+             b is the batch size and
              n is the number of elements in the sequence
-    - m : integer, number of elements in the vocabulary 
+    - m : integer, number of elements in the vocabulary
                 such that x[i,j] <= m-1 for all i,j
 
-    Output:     
+    Output:
     - x_one_hot : np.array of one-hot encoded integers with shape (b,m,n)
 
-                    x[i,j,k] = 1 if x[i,j] = k, else 0 
+                    x[i,j,k] = 1 if x[i,j] = k, else 0
                     for all i,j
     """
 
     b, n = x.shape
 
-    #Making sure that x is an array of integers
+    # Making sure that x is an array of integers
     x = x.astype(np.intc)
-    x_one_hot = np.zeros((b,m,n))
+    x_one_hot = np.zeros((b, m, n))
     # x_one_hot[np.arange(b)[:][None]][x][np.arange(n)[None][:]] = 1
     for i in range(b):
         for j in range(n):
