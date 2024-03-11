8ab8bfffc85549048788cddacda0891eb62d790b
Kajrakso <ofjakob@gmail.com>
2024-03-11 15:10:28 +0100

    småpirk

--- src/test_network.py
+++ src/test_network.py
@@ -16,21 +16,24 @@ def test_trained_network(
         num_ints: size of vocabulary (m in the project description)
     """
     num_batches_x, batch_size_x, n = x_test.shape
-    num_batches_y, batch_size_y, r = y_test.shape
+    _, _, r = y_test.shape
 
     print("\nstart testing...\n")
     print(f"{'batch nr.':>15} | {'percentage correct':>15}")
     print("-" * 40)
 
-    # prepare a x matrix for holding the sequences (3.2.2 in proj. desc.)
+    # prepare a matrix for holding the sequences (3.2.2 in proj. desc.)
     x = np.zeros((batch_size_x, n + r))
+
     for i in range(num_batches_x):
         x[:, :n] = x_test[i]
+
         for j in range(r):
-            Z = network.forward(onehot(x[:, : (n + j)], m=num_ints))
+            Z = network.forward(onehot(x[:, : n + j], m=num_ints))
             x[:, n + j] = np.argmax(Z, axis=1)[:, -1]
         y = x[:, -r:]
 
         num_correct = np.sum(np.all(y_test[i] == y, axis=1))
         print(f"{i+1:>15} | {num_correct/batch_size_x:.3%}")
+
     print("\nend of testing...\n")
